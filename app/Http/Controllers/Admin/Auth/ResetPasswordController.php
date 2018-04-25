a57aa99f040f55c0fcd0c5da87903352ce1161bf
bichitra12 <38726815+bichitra12@users.noreply.github.com>
2018-04-25 09:23:17 -0700

    Add files via upload

---
+++ app/Http/Controllers/Admin/Auth/ResetPasswordController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
+
+class ResetPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+
+    use ResetsPasswords;
+
+    /**
+     * Where to redirect users after resetting their password.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function showAdminResetForm(Request $request, $token = null)
+    {
+        return view('admin.resetpassword')->with(
+            ['token' => $token, 'email' => $request->email]
+            );
+    }
+    public function reset(Request $request)
+    {
+        $this->validate($request, $this->rules(), $this->validationErrorMessages());
+        
+      
+        $response = $this->broker()->reset(
+            $this->credentials($request), function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+            );
+        
+       
+        return $response == Password::PASSWORD_RESET
+        ? $this->sendResetResponse($response)
+        : $this->sendResetFailedResponse($request, $response);
+    }
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+    public function broker()
+    {
+        return Password::broker('admins');
+    }
+    protected function credentials(Request $request)
+    {
+        return $request->only(
+            'email', 'password', 'password_confirmation' 
+            );
+    }
+    protected function resetPassword($user, $password)
+    {
+        $user->forceFill([
+            'password' => bcrypt($password),
+            'remember_token' => Str::random(60),
+        ])->save();
+        
+        $this->guard()->login($user);
+    }
+    protected function guard()
+    {
+        return Auth::guard('admin');
+    }
+    
+}
---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DashboardController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.dashboard');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    
+    }
+    public function showChangePasswordForm(){
+        return view('admin.changepassword');
+    }
+    public function changePassword(Request $request){
+        
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+            // The passwords matches
+            return redirect()->back()->with("error","Your current password does not matches with the password you provided. Please try again.");
+        }
+        
+        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+            //Current password and new password are same
+            return redirect()->back()->with("error","New Password cannot be same as your current password. Please choose a different password.");
+        }
+        
+       /* $validatedData = $request->validate([
+            'current-password' => 'required',
+            'new-password' => 'required|string|min:6|confirmed',
+        ]);*/
+        
+        //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new-password'));
+        $user->save();
+        
+        return redirect()->back()->with("success","Password changed successfully !");
+    }
+    public function EditProfileForm(){
+        $admin = Auth::user();
+        
+        return view('admin.EditProfile',compact('admin'));
+    }
+    public function EditProfileFormSubmit(Request $request){
+        $id = Auth::user()->id;
+        $this->validate($request, [
+            'email' => 'required|email|unique:admins',
+            'mobile' => 'required|unique:admins',
+        ]);
+        
+       
+                DB::table('admins')
+                ->where('id', $id)
+                ->update(['name' => $request->input('name'),'email' => $request->input('email'),'mobile' => $request->input('mobile')]);
+        return redirect()->back()->with('success', 'Record updated successfully');
+        
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    
+}
